9b85929c164516245ffb2bbf2e9a9cdae1e3c264
Alastair Dick <alastaid@microsoft.com>
2018-04-13 16:12:37 +0100

    changed home/about

--- home.php
+++ home.php
@@ -1,30 +1,52 @@
-<?php 
-	session_start();
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Pocklington Squash Club</title>
 <meta content="Pocklington, Squash, Sports" name="keywords" />
 <link rel="stylesheet" type="text/css" href="CSS/psmain.css" />
-
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>About</title>
 </head>
-	<body>
-		<?php include ("headerbanner.php")?>
 
-		<div class="body row scroll-y" align="center" id="MainBody" >
-		<iframe src="courts.html" height="200" width="100%" style="overflow:hidden" marginheight="0" marginwidth="0" frameborder="0"></iframe>
-		<a class="twitter-timeline"  width="300" height="500" href="https://twitter.com/pocksquash"  data-widget-id="419907129563041792">Tweets by @pocksquash</a>
-    	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		</br>
-		</br>
-		
-		
+<body>
+<?php 
+include ("headerbanner.php")
+?>
 
-		
-		</div>
-		<?php include ("footer.php")?>
-	</body>
+<div class="body row scroll-y" align="center" id="MainBody" >
+<p>
+
+</p>
+<p>
+We are a small friendly club based out of the <a href="http://www.francisscaifesportscentre.co.uk/">Francis Scaife Sports Centre</a> in Pocklington.   The Francis Scaife Sports Centre is a 
+council run facility which has two glass backed courts that can be booked in 40 minute sessions over the phone. 
+There is no subscription fee to use the courts, and each session is £7.40 per 40 
+minutes for adults, and £4.80 for juniors. If you can play squash during the day it is worth considering getting a 
+surepass card, as this includes use of the squash courts as well as the gym, 
+swimming pool and other facilities, for more information contact the Francis 
+Scaife Sports Centre.</p>
+<p>
+Subscription to the Pocklington Squash Club is &pound;20 per annum for adults and &pound;10 for juniors,
+ membership forms are available from the Squash Club noticeboard which is behind the squash courts at the Francis Scaife Sports Centre.
+</p>
+<p>
+The club currently runs two mens teams in the <a href="http://yorkshiresquash.countyleagues.co.uk/">York & District Leagues</a>, and one womens team.  We also run mini-leagues every month, 
+and a series of competitions at the end of the season designed for everyone to join in,  meet other players and have some fun playing
+squash.  As well as this we run occasional club nights and social nights, details are on the notice board.
+</p>
+<h2>Squash 57 (Racketball)</h2>
+<p>Squash 57 is a fast growing sport, and is ideal for those who want to play something a little 
+kinder on the knees than squash! You can book the courts to play Racketball, and borrow rackets and 
+a ball from the Francis Scaife reception.  </p>
+<h2>Racket Restringing</h2>
+<p>Matthew Dick offers a racket restringing service at the club. He restrings rackets using Ashaway Supernick string, the same string
+used by the professionals. The service costs £20 per racket, and should take no more than a few days. You can find Matthew's contact 
+details on the Members page.</p>
+
+</div>
+<?php include ("footer.php")?>
+
+
+</body>
+
 </html>
