f298755ba5856b3b514707876d6fc38ca5b5b4ad
Alastair Dick <alastaid@microsoft.com>
2018-04-13 15:44:05 +0100

    editing about

--- about.php
+++ about.php
@@ -26,19 +26,18 @@ surepass card, as this includes use of the squash courts as well as the gym,
 swimming pool and other facilities, for more information contact the Francis 
 Scaife Sports Centre.</p>
 <p>
-Subscription to the Pocklington Squash Club is &pound;20 per annum for adults and &pound;10 for juniors, membership forms are available from the Squash Club noticeboard which is behind the squash courts at the Francis Scaife Sports Centre.
+Subscription to the Pocklington Squash Club is &pound;20 per annum for adults and &pound;10 for juniors,
+ membership forms are available from the Squash Club noticeboard which is behind the squash courts at the Francis Scaife Sports Centre.
 </p>
 <p>
 The club currently runs two mens teams in the <a href="http://yorkshiresquash.countyleagues.co.uk/">York & District Leagues</a>, and one womens team.  We also run mini-leagues every month, 
 and a series of competitions at the end of the season designed for everyone to join in,  meet other players and have some fun playing
 squash.  As well as this we run occasional club nights and social nights, details are on the notice board.
 </p>
-</p>
-
 <h2>Squash 57 (Racketball)</h2>
 <p>Squash 57 is a fast growing sport, and is ideal for those who want to play something a little 
 kinder on the knees than squash! You can book the courts to play Racketball, and borrow rackets and 
-a ball from the Francis Scaife reception.  
+a ball from the Francis Scaife reception.  </p>
 <h2>Racket Restringing</h2>
 <p>Matthew Dick offers a racket restringing service at the club. He restrings rackets using Ashaway Supernick string, the same string
 used by the professionals. The service costs £20 per racket, and should take no more than a few days. You can find Matthew's contact 
