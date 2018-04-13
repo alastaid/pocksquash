020d1823a1fba4bec3cd40ef5def0023d8adbabb
Alastair Dick <alastaid@microsoft.com>
2018-04-13 15:34:40 +0100

    updated about

--- about.php
+++ about.php
@@ -20,55 +20,25 @@ include ("headerbanner.php")
 <p>
 We are a small friendly club based out of the <a href="http://www.francisscaifesportscentre.co.uk/">Francis Scaife Sports Centre</a> in Pocklington.   The Francis Scaife Sports Centre is a 
 council run facility which has two glass backed courts that can be booked in 40 minute sessions over the phone. 
-There is no subscription fee to use the courts, and each session is £7.00 per 40 
+There is no subscription fee to use the courts, and each session is £7.40 per 40 
 minutes for adults, and £4.80 for juniors. If you can play squash during the day it is worth considering getting a 
 surepass card, as this includes use of the squash courts as well as the gym, 
 swimming pool and other facilities, for more information contact the Francis 
 Scaife Sports Centre.</p>
 <p>
-Subscription to the Pocklington Squash Club is &pound;20 per annum for adults and &pound;10 for juniors, included in this fee is the affiliation 
-to <a href="http://www.englandsquashandracketball.com/">England Squash & Racketball</a>,  which also offers a series of benefits.  Membership runs from July to June, and membership
-forms are available from the Squash Club noticeboard which is behind the squash courts at the Francis Scaife Sports Centre.
+Subscription to the Pocklington Squash Club is &pound;20 per annum for adults and &pound;10 for juniors, membership forms are available from the Squash Club noticeboard which is behind the squash courts at the Francis Scaife Sports Centre.
 </p>
 <p>
-The club currently runs three mens teams in the <a href="http://yorkshiresquash.countyleagues.co.uk/">York & District Leagues</a>, and one womens team.  We also run mini-leagues every month, 
+The club currently runs two mens teams in the <a href="http://yorkshiresquash.countyleagues.co.uk/">York & District Leagues</a>, and one womens team.  We also run mini-leagues every month, 
 and a series of competitions at the end of the season designed for everyone to join in,  meet other players and have some fun playing
 squash.  As well as this we run occasional club nights and social nights, details are on the notice board.
 </p>
-<h2>Coaching</h2>
-<p>
-Junior and adult Coaching is provided by Derek Norris, a level 3 England Squash and Racketball registered
- coach who is experienced at coaching at all levels from beginner to county standard players.
 </p>
-<h4>Junior/Youth squash:</h4>
-<p>
-Junior and Youth squash coaching aims to develop skills, improve co-ordination and fitness and to be fun 
-and inspire a love of the game. Classes for Junior/youth players (age 6 upwards) run at Francis Scaife 
-Sports Centre on Mondays and Thursdays.  Private group or individual coaching can also be arranged.
-On Sunday morning we have a junior/youth morning from 09.40-11.00.    Derek is a member of the coaching 
-team at the <a href="http://www.hjsa.org">Humber Junior Squash Academy</a> and the best players across the Humber region meet 
-quarterly to compete in the Humber rankings and other events.  A number of Pocklington players have already 
-been invited to participate in these events.
-</p>
-<p>
-We get occasional funding to run extra coaching events.  If you would like to be kept updated of Junior 
-and youth coaching and training opportunities, please ask e-mail Derek on dereknorris@live.com and ask 
-to be put on the mailing list. 
-</p>
-<h4>Adult Squash:</h4>
-<p>
-A group adult coached session is held on a Thursday evening, and private individual or group sessions 
-can be arranged on request. Team coaching/training is facilitated by Derek on Sunday mornings from 
-11.00-12.20.
-</p>
-<p>For further information about all activities please contact Derek on 07882440329. 
-</p>
-<h2>Racketball</h2>
-<p>Racketball is a fast growing sport, and is ideal for those who want to play something a little 
+
+<h2>Squash 57 (Racketball)</h2>
+<p>Squash 57 is a fast growing sport, and is ideal for those who want to play something a little 
 kinder on the knees than squash! You can book the courts to play Racketball, and borrow rackets and 
-a ball from the Francis Scaife reception.  Derek also runs a Racketball session on a Thursday afternoon 
-from 1.30-3.30 and players of all abilities are welcome.  To attend this session please book through 
-Francis Scaife sports centre (01759 305052).</p>
+a ball from the Francis Scaife reception.  
 <h2>Racket Restringing</h2>
 <p>Matthew Dick offers a racket restringing service at the club. He restrings rackets using Ashaway Supernick string, the same string
 used by the professionals. The service costs £20 per racket, and should take no more than a few days. You can find Matthew's contact 
